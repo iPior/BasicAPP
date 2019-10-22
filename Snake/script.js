688e3e6dd387d5d534a14b7a690a4858b0f6fe14
Vivekanand Gadhia <vivekanandgadhia@user3-225-217.wireless.utoronto.ca>
2019-10-22 13:52:41 -0400

    fixed snake length bug

--- Snake/script.js
+++ Snake/script.js
@@ -1,64 +1,10 @@
 const score_div = document.getElementById("game-score")
-//Snake Class
-function Snake(){
-	this.x = 360;
-	this.y = 320;
-	this.xspeed = 0;
-	this.yspeed = 0;
-	this.score = 0;
-	this.trail = [];
-
-	this.collision = function(pos){
-		var d = dist(this.x, this.y, pos.x, pos.y)
-		if (d <1){
-			this.total++;
-			return true;
-		}
-		return false;
-	}
-
-	this.dir = function (x, y){
-		this.xspeed = x; 
-		this.yspeed = y;
-	}
-	this.end = function(){
-		for (var i = 0; i< this.trail.length; i++){
-			var position = this.trail[i];
-			if (this.collision(pos)){
-				//game over
-				this.score = 0;
-				this.trail = [];
-			}
-		}
-	}
-	this.update = function(){
-		for(var i = 0; i<this.trail.length-1; i++){
-			this.trail[i] = this.trail[i+1];
-		}
-		//idk why we do this????
-		if(this.score>=1){
-			this.trail[this.score-1] = createVector(this.x, this.y);
-		}
-		this.x = this.x + this.xspeed * size;
-    	this.y = this.y + this.yspeed * size;
-
-    	this.x = constrain(this.x, 0, width - size);
-   		this.y = constrain(this.y, 0, height - size);
-	}
-
-	this.draw = function(){
-		fill(255);
-		for (var i = 0; i< this.trail.length; i++){
-			rect(this.trail[i].x, this.trail[i].y, size, size);
-		}
-		rect(this.x, this.y, size, size);
-	}
-}
 //Main Game
 var s;
 var size = 10;
 var food;
 var canvas;
+var score = 0;
 
 function setup(){
 	canvas = createCanvas(800,450);
@@ -80,9 +26,12 @@ function draw(){
 
 	if(s.collision(food)){
 		updateFood();
+		score++;
 	}
-	s.end();
-	s.update();
+	if(s.end()){
+		score = 0;
+	}
+	s.update(score);
 	s.draw();
 
 	fill(255, 0, 100);
@@ -100,4 +49,60 @@ function keyPressed(){
   } else if (keyCode === LEFT_ARROW) {
     s.dir(-1, 0);
   }
+}
+//Snake Class
+function Snake(){
+	this.x = 360;
+	this.y = 320;
+	this.xspeed = 0;
+	this.yspeed = 0;
+	//this.score = 0;
+	this.trail = [];
+
+	this.collision = function(pos){
+		var d = dist(this.x, this.y, pos.x, pos.y)
+		if (d <1){
+			//this.score++;
+			return true;
+		}
+		return false;
+	}
+
+	this.dir = function (x, y){
+		this.xspeed = x; 
+		this.yspeed = y;
+	}
+	this.end = function(){
+		for (var i = 0; i< this.trail.length; i++){
+			var position = this.trail[i];
+			if (this.collision(position)){
+				//game over
+				//this.score = 0;
+				this.trail = [];
+				return true;
+			}
+		}
+		return false;
+	}
+	this.update = function(score){
+		for(var i = 0; i<this.trail.length-1; i++){
+			this.trail[i] = this.trail[i+1];
+		}
+		if(score>=1){
+			this.trail[score-1] = createVector(this.x, this.y);
+		}
+		this.x = this.x + this.xspeed * size;
+    	this.y = this.y + this.yspeed * size;
+
+    	this.x = constrain(this.x, 0, width - size);
+   		this.y = constrain(this.y, 0, height - size);
+	}
+
+	this.draw = function(){
+		fill(255);
+		for (var i = 0; i< this.trail.length; i++){
+			rect(this.trail[i].x, this.trail[i].y, size, size);
+		}
+		rect(this.x, this.y, size, size);
+	}
 }
